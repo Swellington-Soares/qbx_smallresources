d3d829b631dd850ec480d6a47489b4af6007e888
Swellington Soares <qb.corebr@gmail.com>
2024-10-24 11:04:26 -0300

    change config

--- qbx_hudcomponents/config.lua
+++ qbx_hudcomponents/config.lua
@@ -1,10 +1,10 @@
 return {
     disable = {
         -- https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
-        hudComponents = {1, 2, 3, 4, 7, 9, 13, 14, 19, 20, 21, 22},
+        hudComponents = {1, 2, 3, 4, 7, 9, 13, 19, 20, 21, 22},
 
         -- https://docs.fivem.net/docs/game-references/controls/
-        controls = {37},
+        controls = {37, 36},
 
         -- False disables ammo display
         displayAmmo = true,
